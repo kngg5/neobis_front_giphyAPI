7cd0a6ee16d7087bcab4097ac6f4815d395dce5f
kngg5 <yasinmamatov12@gmail.com>
2023-05-06 03:53:31 +0600

    Changed location of api key

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
+
 const main = document.querySelector(".main");
 const input = document.querySelector("#giphy_name");
 
-const api_key = 'AIFx8rb2a9vH35WGBl1LuszIu1Tqo9ew';
+
 const createGiph = () => {
     const input = document.querySelector("#giphy_name").value;
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${input}&limit=21`;    
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_GET_API_KEY}&q=${input}&limit=21`;    
+    
     main.innerHTML= '';
     fetch(url)
-       
+        .then((response) => response.json())
+        .then((gif) => {
+           
+            const gifItems = gif.data;
+            gifItems.forEach((element) => {
+                
+                const gifImage = document.createElement("img");
+                gifImage.classList.add("gifImg");
+                gifImage.setAttribute("src",element.images.fixed_width.url);
+                
+               
+                const gifContainer = document.querySelector(".main"); 
+                gifContainer.append(gifImage);
+            });
+        })
         .catch((error) => {
             console.error(error);
         })
@@ -16,4 +32,4 @@ const createGiph = () => {
 input.addEventListener("change", () => {
     createGiph()
 });
-createGiph();
+createGiph()
