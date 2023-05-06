58df60f92f20fcf5ae1a53150445cde75aa2f7a7
kngg5 <yasinmamatov12@gmail.com>
2023-05-06 13:37:14 +0600

    Env file issue was solved

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 
 const main = document.querySelector(".main");
 const input = document.querySelector("#giphy_name");
-
+const api_key = `AIFx8rb2a9vH35WGBl1LuszIu1Tqo9ew`;
 
 const createGiph = () => {
     const input = document.querySelector("#giphy_name").value;
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.GIPHY_GET_API_KEY}&q=${input}&limit=21`;    
+    
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${api_key}&q=${input}&limit=21`;    
     
     main.innerHTML= '';
     fetch(url)
